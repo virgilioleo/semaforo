2d1de01e2ec391b4bfb657ddac0933b1f5050240
Virgilioleo <leosilveira@outlook.com>
2025-04-04 05:53:08 -0300

    Primeiro commit

---
+++ semaforo.js
@@ -0,0 +1,8 @@
+const img = document.getElementById('img');
+const buttons = document.getElementById('buttton');
+
+const trafficLight = (event) => {
+
+}
+
+buttons.addEventListener('click', trafficLight);
